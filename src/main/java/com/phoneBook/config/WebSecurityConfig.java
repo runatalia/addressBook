9cc737e6f37e5f20e1db8d66fb52cc9da13cb2ce
runatalia <voroninana1990@mail.ru>
2023-01-17 01:55:55 +0300

    security

--- src/main/java/com/phoneBook/config/WebSecurityConfig.java
+++ src/main/java/com/phoneBook/config/WebSecurityConfig.java
@@ -4,10 +4,15 @@ import com.phoneBook.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,45 +23,47 @@ public class WebSecurityConfig {
     UserService userService;
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public PasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
     
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http 
-                .csrf()
-                .disable()
-                .authorizeRequests()
-                //Доступ только для не зарегистрированных пользователей
-//                .antMatchers("/registration").not().fullyAuthenticated()
-//                //Доступ только для пользователей с ролью Администратор
-//                .antMatchers("/admin/**").hasRole("ADMIN")
-//                .antMatchers("/news").hasRole("USER")
-//                //Доступ разрешен всем пользователей
-//                .antMatchers("/", "/resources/**").permitAll()
-//                //Все остальные страницы требуют аутентификации
-                .anyRequest().authenticated()
-                .and()
-                //Настройка для входа в систему
-                .formLogin()
-                .loginPage("/login")
-                //Перенарпавление на главную страницу после успешного входа
-                .defaultSuccessUrl("/")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll()
-                .logoutSuccessUrl("/");
 
-        return http.build();
+    
+     @Bean
+     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http .csrf().disable().authorizeHttpRequests((requests) -> requests
+            .requestMatchers("/**")
+            .permitAll()
+        )
+        .formLogin((form) -> form
+            .loginPage("/login").permitAll()
+        )
+        .logout((logout) -> logout.permitAll());
+    
+    return http.build();
+    
+
     }
 
-   
+  
+    
+    @Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("u")
+				.password("p")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
     
 //        @Autowired
 //    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 //        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+//         
 //    }
 }
 
--- src/main/java/com/phoneBook/controller/RestControllerPhoneBook.java
+++ src/main/java/com/phoneBook/controller/RestControllerPhoneBook.java
@@ -44,6 +44,8 @@ public class RestControllerPhoneBook {
     
     
     
+    
+    
    
 
 //    @PostMapping("/employees")
